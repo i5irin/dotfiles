68cccc3c0a49df4f1dd917cad1bbe4e3a0165634
i5irin <contact@i5ir.in>
2019-02-08 23:29:29 +0900

    VSCodeの設定用のスクリプトを修正

--- vscode/vscode-setup.sh
+++ vscode/vscode-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+readonly SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE}); pwd)
+
 # install extensions
-for extension in `cat extensions`; do
+for extension in $(cat "${SCRIPT_PATH}/extensions"); do
     code --install-extension $extension
 done
 
 # link setting.json
 # modify ./setting.json to configure VisualStudioCode common setting
-ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
+ln -sf "${SCRIPT_PATH}/settings.json" ~/Library/Application\ Support/Code/User/
