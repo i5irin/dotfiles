67c7436a6bbe238c48941abab582327e44f053f3
i5irin <contact@i5ir.in>
2019-11-02 21:57:50 +0900

    Hyperのカーソルの点滅設定と見た目に関する拡張機能一覧を更新

--- .hyper.js
+++ .hyper.js
@@ -36,7 +36,7 @@ module.exports = {
         cursorShape: 'BLOCK',
 
         // set to `true` (without backticks and without quotes) for blinking cursor
-        cursorBlink: false,
+        cursorBlink: true,
 
         // color of the text
         foregroundColor: '#fff',
@@ -137,7 +137,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-teatime"],
+    plugins: ["hyper-snazzy", "hyper-transparent"],
 
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
