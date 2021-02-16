d64462b527c7f6937acdfb5ab9cbd57131031cb2
i5irin <contact@i5ir.in>
2021-02-17 03:54:20 +0900

    Remove Hyper
    Remove it because updates are not provided.

--- install.sh
+++ install.sh
@@ -82,12 +82,6 @@ source "${INSTALL_SCRIPT_PATH}/macos/macos-preferences.sh"
 source "${INSTALL_SCRIPT_PATH}/vscode/vscode-setup.sh"
 
 # ---------------------------------------------------------
-# Configure Hyper (terminal emulator)
-# ---------------------------------------------------------
-
-ln -is "${INSTALL_SCRIPT_PATH}~/dotfiles/.hyper.js" ~/.hyper.js
-
-# ---------------------------------------------------------
 # Configure Karabiner-Elements
 # ---------------------------------------------------------
 
