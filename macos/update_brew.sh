24e5e0c747f43e016ef6d89b8171c914652f0476
i5irin <contact@i5ir.in>
2021-11-14 06:21:30 +0900

    Improve brew tasks to have separate log levels for stdout and errors

--- macos/update_brew.sh
+++ macos/update_brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-{
+{ {
   echo 'brew doctor ---------------------------------------------------';
   if brew doctor; then
     echo 'brew update ---------------------------------------------------';
@@ -10,4 +10,4 @@
     # The "brew cleanup" is automatically executed every 30 days by default in Homebrew,
     # and is omitted here in consideration of problems that may occur after a package update.
   fi
-} 2>&1 | logger -ip 'user.info'
+} 2>&1 1>&3 3>&- | logger -ip 'user.warning'; } 3>&1 1>&2 | logger -ip 'user.notice'
