0cbfdd859be115ed2bbf73cc8c3eee3068c7f9c0
i5irin <contact@i5ir.in>
2021-11-22 00:38:14 +0900

    Add config to periodically update apps installed from Mac App Store

--- macos/update_applications.sh
+++ macos/update_applications.sh
@@ -10,4 +10,7 @@
     # The "brew cleanup" is automatically executed every 30 days by default in Homebrew,
     # and is omitted here in consideration of problems that may occur after a package update.
   fi
+  # WARNING: Update applications installed from the Mac App Store, but some of them cannot be updated.
+  echo 'mas upgrade ---------------------------------------------------'
+  mas upgrade
 } 2>&1 1>&3 3>&- | logger -ip 'user.warning'; } 3>&1 1>&2 | logger -ip 'user.notice'
