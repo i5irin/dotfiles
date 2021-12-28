b00a6a014a9ccb774464dcbcef275997fc7aa4b5
i5irin <contact@i5ir.in>
2021-12-29 00:19:43 +0900

    Add a setup script for Ubuntu

---
+++ ubuntu/setup_ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+readonly INSTALL_SCRIPT_PATH=$(cd "$(dirname ${BASH_SOURCE})../"; pwd)
+
+# ---------------------------------------------------------
+# Configure Bash
+# ---------------------------------------------------------
+
+# link .bash_profile and .bashrc
+ln -is "${INSTALL_SCRIPT_PATH}/.bash_profile" ~/.bash_profile
+ln -is "${INSTALL_SCRIPT_PATH}/.bashrc" ~/.bashrc
+
+# link readline config
+ln -s "${INSTALL_SCRIPT_PATH}/.inputrc" ~/.inputrc
