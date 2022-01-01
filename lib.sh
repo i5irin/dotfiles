c4d44e6e556d693577f926eb4c7ad825c3492def
i5irin <contact@i5ir.in>
2022-01-02 00:53:16 +0900

    Add a function to use paths relative to the entity from symbolic links

--- lib.sh
+++ lib.sh
@@ -42,3 +42,64 @@ validate_github_username() {
   echo "The username you entered is invalid for GitHub." 1>&2
   return 1
 }
+
+########################################################################
+# POSIX-compliant readlink command with the -f option.
+# Arguments:
+#   Path of symbolic link
+# Outputs:
+#   Path of the symbolic link entity
+# Returns:
+#   Whether or not the function has output the entity of the symbolic links
+# Note:
+#   This function is based on ko1nksm's readlinkf. See this following link for more details.
+#   https://github.com/ko1nksm/readlinkf
+########################################################################
+readlinkf() {
+  if [ $# -eq 0 ]; then
+    echo "readlink: missing operand" >&2
+    return 1
+  fi
+
+  readlinkf_one_path() {
+    [ "${1:-}" ] || return 1
+    max_symlinks=40
+    CDPATH='' # to avoid changing to an unexpected directory
+
+    target=$1
+    [ -e "${target%/}" ] || target=${1%"${1##*[!/]}"} # trim trailing slashes
+    [ -d "${target:-/}" ] && target="$target/"
+
+    cd -P . 2>/dev/null || return 1
+    while [ "$max_symlinks" -ge 0 ] && max_symlinks=$((max_symlinks - 1)); do
+      if [ ! "$target" = "${target%/*}" ]; then
+        case $target in
+          /*) cd -P "${target%/*}/" 2>/dev/null || break ;;
+          *) cd -P "./${target%/*}" 2>/dev/null || break ;;
+        esac
+        target=${target##*/}
+      fi
+
+      if [ ! -L "$target" ]; then
+        target="${PWD%/}${target:+/}${target}"
+        printf '%s\n' "${target:-/}"
+        return 0
+      fi
+
+      # `ls -dl` format: "%s %u %s %s %u %s %s -> %s\n",
+      #   <file mode>, <number of links>, <owner name>, <group name>,
+      #   <size>, <date and time>, <pathname of link>, <contents of link>
+      # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/ls.html
+      link=$(ls -dl -- "$target" 2>/dev/null) || break
+      target=${link#*" $target -> "}
+    done
+    return 1
+  }
+
+  ex=0
+  for i; do
+    (readlinkf_one_path "$i") || ex=1
+  done
+  unset readlinkf_one_path
+  return "$ex"
+}
