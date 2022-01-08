53a6873956298e926e75db0cf115e4fd62d83613
i5irin <contact@i5ir.in>
2022-01-08 23:38:48 +0900

    Fix the setup script not creating the .bashrc symbolic link for Ubuntu

--- ubuntu/setup_ubuntu.sh
+++ ubuntu/setup_ubuntu.sh
@@ -8,7 +8,7 @@ readonly INSTALL_SCRIPT_PATH=$(cd "$(dirname ${BASH_SOURCE})../"; pwd)
 
 # link .bash_profile and .bashrc
 ln -is "${INSTALL_SCRIPT_PATH}/.bash_profile" ~/.bash_profile
-ln -is "${INSTALL_SCRIPT_PATH}/.bashrc" ~/.bashrc
+ln -is "${INSTALL_SCRIPT_PATH}/ubuntu/.bashrc_ubuntu" ~/.bashrc
 
 # link readline config
 ln -s "${INSTALL_SCRIPT_PATH}/.inputrc" ~/.inputrc
