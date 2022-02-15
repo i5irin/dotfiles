c3d2283e37d8115d129a8b7f61378d1a50127a3a
i5irin <contact@i5ir.in>
2022-02-16 07:33:12 +0900

    Move the preferences from setup script for Windows to a separate file

--- windows/PreferencesWindows.ps1
+++ windows/PreferencesWindows.ps1
@@ -1,22 +1,3 @@
-# Check whether this script is running with administrative privileges.
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-  Write-Output 'This script should be run with administrative privileges.'
-  exit 1
-}
-
-Set-Variable -Name INSTALL_SCRIPT_PATH -Value $(Convert-Path "${PSScriptRoot}\..") -Option ReadOnly
-
-# Load the library functions.
-Import-Module "${INSTALL_SCRIPT_PATH}\lib\WindowsDotfilesUtils"
-
-# Install WSL.
-# Check if the build number is 19041 or later.
-if ((Get-WmiObject Win32_OperatingSystem).BuildNumber -lt 19041) {
-  Write-Output 'To install WSL2 with this script, Upgrade to Windows build 19041 or later.'
-  exit 1
-}
-wsl --install
-
 # Activate Developer Mode.
 Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock' -Name 'AllowDevelopmentWithoutDevLicense' -Value '1'
 
@@ -54,6 +35,7 @@ Get-AppxPackage "Microsoft.MSPaint" | Remove-AppxPackage
 Enable-RegistryKey -Name 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
 
 # Disable storing passwords.
+# Recommendedは設定をUI側でするときの文言が変わる程度CLIから設定するのであればRecommendedは使わないほうがいい
 Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Name 'PasswordManagerEnabled' -Value 0
 
 # Disable auto-filling your address.
@@ -61,24 +43,3 @@ Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Name 'Au
 
 # Disable auto-filling your credit card information.
 Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Name 'AutofillCreditCardEnabled' -Value 0
-
-# Install applications.
-
-winget import -i "${INSTALL_SCRIPT_PATH}\windows\apps.json"
-
-# Configure Git
-# NOTE: Git can be installed on both WSL and Windows. This script will install Git on Windows and set up a .gitconfig for it.
-Import-Module "${INSTALL_SCRIPT_PATH}\git\SetupGitWindows"
-Receive-GitConfig -Path "${INSTALL_SCRIPT_PATH}\git\.gitconfig"
-
-# Register periodic tasks.
-
-if (![System.Diagnostics.EventLog]::SourceExists('WingetUpdate')) {
-New-EventLog -LogName Application -Source WingetUpdate
-}
-if ((Get-ScheduledTask -TaskName "UpdateWingetPackages" -ErrorAction SilentlyContinue) -eq $null) {
-# Register the update of the winget package in the task scheduler.
-Register-ScheduledTask -TaskName 'UpdateWingetPackages' -Description 'Update the packages installed by winget.' `
-  -Action (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File ${INSTALL_SCRIPT_PATH}\windows\UpdatePackages.ps1") `
-  -Trigger (New-ScheduledTaskTrigger -Daily -At '00:00')
-}
---
+++ windows/SetupWindows.ps1
@@ -0,0 +1,45 @@
+# Check whether this script is running with administrative privileges.
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  Write-Output 'This script should be run with administrative privileges.'
+  exit 1
+}
+
+Set-Variable -Name INSTALL_SCRIPT_PATH -Value $(Convert-Path "${PSScriptRoot}\..") -Option ReadOnly
+
+# Load the library functions.
+Import-Module "${INSTALL_SCRIPT_PATH}\lib\WindowsDotfilesUtils"
+
+# Install WSL.
+# Check if the build number is 19041 or later.
+if ((Get-WmiObject Win32_OperatingSystem).BuildNumber -lt 19041) {
+  Write-Output 'To install WSL2 with this script, Upgrade to Windows build 19041 or later.'
+  exit 1
+}
+wsl --install
+
+# Install applications.
+
+winget import -i "${INSTALL_SCRIPT_PATH}\Windows\apps.json"
+
+# ---------------------------------------------------------
+# Configure Windows preference
+# ---------------------------------------------------------
+
+& "${INSTALL_SCRIPT_PATH}\Windows\PreferencesWindows.ps1"
+
+# Configure Git
+# NOTE: Git can be installed on both WSL and Windows. This script will install Git on Windows and set up a .gitconfig for it.
+Import-Module "${INSTALL_SCRIPT_PATH}\git\SetupGitWindows"
+Receive-GitConfig -Path "${INSTALL_SCRIPT_PATH}\git\.gitconfig"
+
+# Register periodic tasks.
+
+if (![System.Diagnostics.EventLog]::SourceExists('WingetUpdate')) {
+  New-EventLog -LogName Application -Source WingetUpdate
+}
+if ((Get-ScheduledTask -TaskName "UpdateWingetPackages" -ErrorAction SilentlyContinue) -eq $null) {
+  # Register the update of the winget package in the task scheduler.
+  Register-ScheduledTask -TaskName 'UpdateWingetPackages' -Description 'Update the packages installed by winget.' `
+    -Action (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File ${INSTALL_SCRIPT_PATH}\Windows\UpdatePackages.ps1") `
+    -Trigger (New-ScheduledTaskTrigger -Daily -At '00:00')
+}
