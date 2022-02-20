a71ca0c413ec38cf1c749624f9eff5e76f486f23
i5irin <contact@i5ir.in>
2022-02-21 06:23:55 +0900

    Change the first letter of the folder name for Windows to uppercase

---
+++ Windows/SetupWindows.ps1
@@ -0,0 +1,80 @@
+# Check whether this script is running with administrative privileges.
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  Write-Output 'This script should be run with administrative privileges.'
+  exit 1
+}
+
+Set-Variable -Name INSTALL_SCRIPT_PATH -Value $(Convert-Path "${PSScriptRoot}\..") -Option ReadOnly
+
+# Load the library functions.
+Import-Module "${INSTALL_SCRIPT_PATH}\lib\WindowsDotfilesUtils"
+
+# Install WSL.
+# Check if the build number is 19041 or later.
+if ((Get-WmiObject Win32_OperatingSystem).BuildNumber -lt 19041) {
+  Write-Output 'To install WSL2 with this script, Upgrade to Windows build 19041 or later.'
+  exit 1
+}
+wsl --install
+
+# ---------------------------------------------------------
+# Configure PowerShell
+# ---------------------------------------------------------
+New-Item -Type SymbolicLink ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Value "${INSTALL_SCRIPT_PATH}\Windows\Microsoft.PowerShell_profile.ps1"
+
+# Install Scoop
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+# Install Nerd Fonts
+if (!(Test-PathTest -Path $Env:LOCALAPPDATA\Microsoft\Windows\Fonts)) {
+  New-Item -ItemType Directory $Env:LOCALAPPDATA\Microsoft\Windows\Fonts
+}
+Set-Location $Env:LOCALAPPDATA\Microsoft\Windows\fonts
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Bold/complete/Fira Code Bold Nerd Font Complete Windows Compatible.ttf' -outfile 'Fira Code Bold Nerd Font Complete Windows Compatible.ttf'
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Light/complete/Fira Code Light Nerd Font Complete Windows Compatible.ttf'  -outfile 'Fira Code Light Nerd Font Complete Windows Compatible.ttf'
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Medium/complete/Fira Code Medium Nerd Font Complete Windows Compatible.ttf'  -outfile 'Fira Code Medium Nerd Font Complete Windows Compatible.ttf'
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira Code Regular Nerd Font Complete Windows Compatible.ttf'  -outfile 'Fira Code Regular Nerd Font Complete Windows Compatible.ttf'
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Retina/complete/Fira Code Retina Nerd Font Complete Windows Compatible.ttf'  -outfile 'Fira Code Retina Nerd Font Complete Windows Compatible.ttf'
+Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/SemiBold/complete/Fira Code SemiBold Nerd Font Complete Windows Compatible.ttf'  -outfile 'Fira Code SemiBold Nerd Font Complete Windows Compatible.ttf'
+# Install Starship
+# TODO: Install Starship without Scoop.
+scoop install starship
+
+# ---------------------------------------------------------
+# Install applications
+# ---------------------------------------------------------
+
+# Update the application to be installed according to the user's Winget.json if it exists.
+$baseApplications = (Get-Content -Path "${INSTALL_SCRIPT_PATH}\Windows\Winget.json" | ConvertFrom-Json).Sources[0].Packages | ForEach-Object { $_.PackageIdentifier };
+$installApplications = $baseApplications
+if (Test-PathTest-Path "${INSTALL_SCRIPT_PATH}\Windows\MyWinget.json") {
+  $userApplications = (Get-Content -Path "${INSTALL_SCRIPT_PATH}\Windows\MyWinget.json" | ConvertFrom-Json).Packages | ForEach-Object { $_.PackageIdentifier }
+  $installApplications = ($baseApplications | Where-Object { $userApplications -notcontains $_ }) + ($userApplications | Where-Object { $baseApplications -notcontains $_ })
+}
+$installApplications | ForEach-Object { winget install --id $_ }
+
+# ---------------------------------------------------------
+# Configure Windows preference
+# ---------------------------------------------------------
+
+& "${INSTALL_SCRIPT_PATH}\Windows\PreferencesWindows.ps1"
+
+# Configure Git
+# NOTE: Git can be installed on both WSL and Windows. This script will install Git on Windows and set up a .gitconfig for it.
+Import-Module "${INSTALL_SCRIPT_PATH}\apps\git\SetupGitWindows"
+Receive-GitConfig -Path "${INSTALL_SCRIPT_PATH}\apps\git\.gitconfig"
+
+# Configure Hyper.js
+& "${INSTALL_SCRIPT_PATH}\apps\hyper\SetupHyper.ps1" "${INSTALL_SCRIPT_PATH}\apps\hyper"
+
+# Register periodic tasks.
+
+if (![System.Diagnostics.EventLog]::SourceExists('WingetUpdate')) {
+  New-EventLog -LogName Application -Source WingetUpdate
+}
+if ((Get-ScheduledTask -TaskName "UpdateWingetPackages" -ErrorAction SilentlyContinue) -eq $null) {
+  # Register the update of the winget package in the task scheduler.
+  Register-ScheduledTask -TaskName 'UpdateWingetPackages' -Description 'Update the packages installed by winget.' `
+    -Action (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File ${INSTALL_SCRIPT_PATH}\Windows\UpdatePackages.ps1") `
+    -Trigger (New-ScheduledTaskTrigger -Daily -At '00:00')
+}
