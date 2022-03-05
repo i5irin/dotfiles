325a75e4affadf0dde9a6922c66fa22359c0b5eb
i5irin <contact@i5ir.in>
2022-03-05 22:24:35 +0900

    Add an alias for git init that makes "core.ignorecase=false"

--- alias/alias.sh
+++ alias/alias.sh
@@ -1,3 +1,6 @@
 # Start an Http server that returns "Hello World.
 # Reference Links: http://blog.livedoor.jp/sonots/archives/34703829.html
 alias http_server='while true; do ( echo "HTTP/1.0 200 Ok"; echo; echo "Hello World" ) | nc -l 8080; [ $? != 0 ] && break; done'
+
+# Set "core.ignorecase=false" even for file systems that ignore case.
+alias git-init='git init && git config core.ignorecase false'
