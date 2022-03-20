2fbadcb1cb8f89105eaeb30853e0fe83b15390c3
i5irin <contact@i5ir.in>
2022-03-20 23:11:02 +0900

    Make the application's update log record the date

--- ubuntu/update_applications.sh
+++ ubuntu/update_applications.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+logger -ip '==============================================================='
+logger -ip '    Update applications'
+logger -ip '==============================================================='
+logger -ip "Current time $(date '+%Y-%m-%dT%H:%M:%S%z')"
+
 logger -ip 'user.info' 'apt update ---------------------------------------------------'
 { apt update 2>&1 1>&3 3>&- | logger -ip 'user.warning'; } 3>&1 1>&2 | logger -ip 'user.info'
 logger -ip 'user.info' 'apt upgrade --------------------------------------------------'
