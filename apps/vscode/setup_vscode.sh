037b96916c1ae111e4f6878ec45ae1f2118b4229
i5irin <contact@i5ir.in>
2022-04-08 23:14:40 +0900

    Add Ubuntu support to the VSCode setup script

--- apps/vscode/setup_vscode.sh
+++ apps/vscode/setup_vscode.sh
@@ -2,12 +2,20 @@
 
 set -eu
 readonly VSCODE_SCRIPT_PATH=$1
+readonly PLATFORM=$2
 
 # install extensions
 for extension in $(cat "${VSCODE_SCRIPT_PATH}/extensions"); do
-    code --install-extension $extension
+  code --install-extension $extension
 done
 
 # link setting.json
 # modify ./setting.json to configure VisualStudioCode common setting
-ln -sf "${VSCODE_SCRIPT_PATH}/settings.json" ~/Library/Application\ Support/Code/User/
+if [ $PLATFORM = "macos" ]; then
+  ln -sf "${VSCODE_SCRIPT_PATH}/settings.json" ~/Library/Application\ Support/Code/User/
+elif [ $PLATFORM = "ubuntu" ]; then
+  ln -sf "${VSCODE_SCRIPT_PATH}/settings.json" ~/.config/Code/User/
+else
+  echo 'Unsupported platform.' 1>&2
+  exit 1
+fi
