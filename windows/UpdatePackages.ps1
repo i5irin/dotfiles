fead43dbc45627865054e19ed4657ec8d0b1df82
i5irin <contact@i5ir.in>
2022-06-18 23:43:19 +0900

    Rename the folder once to change the case

--- windows/UpdatePackages.ps1
+++
@@ -1,5 +0,0 @@
-# Update the packages installed by winget.
-
-Write-EventLog -LogName Application -Source WingetUpdate -EventId 0 -EntryType Information -Message 'winget upgrade --all'
-winget upgrade --all
-Write-EventLog -LogName Application -Source WingetUpdate -EventId 0 -EntryType Information -Message 'winget upgrrade completed!'
