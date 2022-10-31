0c577fe764c3d8a7e0069fa792e9cfa32b4b12b7
i5irin <contact@i5ir.in>
2022-10-31 21:39:43 +0900

    Add messages for the start and end of the Clipy setup

--- apps/setup_clipy.sh
+++ apps/setup_clipy.sh
@@ -5,6 +5,9 @@
 # =========================================================
 set -eu
 
+source ~/dotfiles/lib/posix_dotfiles_utils/utils.sh
+
+configure_info 'Clipy'
 if [ -d /Applications/Clipy.app ]; then
   killall Clipy > /dev/null 2>&1
   alived=$?
@@ -13,4 +16,7 @@ if [ -d /Applications/Clipy.app ]; then
   if [ $alived = 0 ]; then
     open /Applications/Clipy.app
   fi
+  finish_configure_message 'Clipy'
+else
+  echo 'Skip setup because Clipy is not installed.' >&2
 fi
