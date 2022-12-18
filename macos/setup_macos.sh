ff1bbb552365025d0d45a14ecef74817600ecbb5
i5irin <contact@i5ir.in>
2022-12-18 21:39:54 +0900

    Fix undefined variable use in macOS setup

--- macos/setup_macos.sh
+++ macos/setup_macos.sh
@@ -60,7 +60,7 @@ fi
 
 # Create a symbolic link for the library to be used by dotfiles placed in symbolic links such as .zshrc and .zprofile.
 if [ ! -e '~/dotfiles/lib/posix_dotfiles_utils/utils.sh' ] && [ ! -h '~/dotfiles/lib/posix_dotfiles_utils/utils.sh' ]; then
-  mkdir -p ~/dotfiles/lib/posix_dotfiles_utils && ln -is "${DOTFILES_PATH}/lib/posix_dotfiles_utils/utils.sh" ~/dotfiles/lib/posix_dotfiles_utils/utils.sh
+  mkdir -p ~/dotfiles/lib/posix_dotfiles_utils && ln -is "${INSTALL_SCRIPT_PATH}/lib/posix_dotfiles_utils/utils.sh" ~/dotfiles/lib/posix_dotfiles_utils/utils.sh
 fi
 
 # link .zprofile and .zshrc
