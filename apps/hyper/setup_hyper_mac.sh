6fa41a7494c62245726d26ff722446879cd219b4
i5irin <contact@i5ir.in>
2023-02-07 00:17:53 +0900

    Add code to create a directory in which to place Hyper's config files

--- apps/hyper/setup_hyper_mac.sh
+++ apps/hyper/setup_hyper_mac.sh
@@ -8,4 +8,5 @@ readonly HYPER_SCRIPT_PATH=$1
 
 # Link .hyper.js
 # Modify dotfiles/apps/hyper/.hyper.js to configure Hyper.js.
+mkdir -p ~/Library/Application\ Support/Hyper/
 ln -sf "${HYPER_SCRIPT_PATH}/.hyper.js" ~/Library/Application\ Support/Hyper/.hyper.js
